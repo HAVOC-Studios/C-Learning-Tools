6b279d3c5f3713e265c62701b763ecd848f30d42
Jay <116266395+HAVOC-Studios@users.noreply.github.com>
2024-09-06 08:55:11 +0100

    Update arithemeticoperators.cpp

--- arithemeticoperators.cpp
+++ arithemeticoperators.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+// Arithemetic Operators are the return result of a specific arithemetic operation (+ - * /)
+
+// Order
+// Parenthesis
+// Multiplication and Division
+// Addiditon and Subtraction
+
+int main() {
+	//int students = 20;
+
+	//students = students + 1;
+	//students ++; +1 to the value (Used in loops)
+	//students +=1; +1 to the value (Can be used anywhere and any value can be added to the number)
+	//students -= 1; -1 to the value ^^
+	//students --; -1 to the value (Used in loops)
+
+	//students = students * 2
+	//students *= 2;
+
+	//students = students / 2;
+	//students /= 2;
+
+	//int remainder = students % 3;
+
+	//std::cout << remainder;
+
+	int students = 6 - 6 + 12 / 2;
+	std::cout << students;
+
+	return 0;
+}
