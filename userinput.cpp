8fc1fb30b0f96121e2a7dc550976b4baa5c9c9c8
Jay <116266395+HAVOC-Studios@users.noreply.github.com>
2024-09-06 09:14:24 +0100

    Update userinput.cpp

--- userinput.cpp
+++ userinput.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+
+// cout << (Insertion operator)
+// cin >> (Extraction Operator)
+
+int main() {
+	std::string name;
+	int age;
+
+	std::cout << "What's your full name?: ";
+
+	std::cout << "What's your age?: ";
+	std::cin >> age;
+
+	std::cout << "Hello " << name << "\n";
+	std::cout << "You are " << age << " years old";
+
+	return 0;
+}
