061dac9f20a2d88aa8d2d717575873c04dbde8b6
Jay <116266395+HAVOC-Studios@users.noreply.github.com>
2024-09-06 08:18:53 +0100

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    // Integer (Whole Number)
+    int age = 21;
+    int year = 2023;
+    int days = 7.5;
+
+    // Double (Number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // Char (Single Character)
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    // Boolean (True or false)
+    bool student = true;
+    bool power = true;
+    bool forSale = true;
+    
+    // String (Objects that represents a sequence of text)
+    std::string name = "Jay";
+    std::string day = "Friday";
+    std::string food = "Pizza";
+    std::string address = "123 Faker Road";
+
+    std::cout << "Hello " << name;
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+}
