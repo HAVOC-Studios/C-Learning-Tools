7b32f7135ce6503f75fe6461c1f7909080fc8943
Jay <116266395+HAVOC-Studios@users.noreply.github.com>
2024-09-06 08:37:15 +0100

    Update namespaces.cpp

--- namespaces.cpp
+++ namespaces.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+// Namespaces provide a solution for preventing name conflicts in large projects. 
+// Each entity needs a unique name. 
+// A namespace allows for identically named enitites as long as the namespaces are different.
+
+using std::cout;
+
+namespace first {
+    int x = 1;
+}
+
+namespace second {
+    int x = 2;
+}
+
+int main() {
+    using namespace first;
+    cout << x;
+    // cout << first::x; // Using the scope resolution operator we can access outside of the function
+    // cout << second::x; // Using the scope resolution operator we can access outside of the function
+    return 0;
+}
