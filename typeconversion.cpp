2c7c7ecb4b62dfc69975509be05b358240fbd958
Jay <116266395+HAVOC-Studios@users.noreply.github.com>
2024-09-06 09:01:25 +0100

    Update typeconversion.cpp

--- typeconversion.cpp
+++ typeconversion.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+// Type conversion is a conversion of a value of one data type to another 
+// Implicit = automatic
+// Explicit = Precede value with new data type
+
+int main() {
+	// Simple example
+	//double x = (int) 3.14;
+	//std::cout << x;
+
+	// 100 in ASCII Table represents as d
+	// Implicit example 
+	//char x = 100; 
+	//std::cout << x;
+	// Or
+	// Explicit example 
+	//std::cout << (char)100;
+
+	int correct = 8;
+	int questions = 10;
+
+	double score = correct / (double)questions * 100;
+
+	std::cout << score << "%";
+
+	return 0;
+}
