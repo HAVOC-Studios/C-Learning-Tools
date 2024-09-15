54013bcabee054a4c98e8892d970e05e6a62b861
Jay <116266395+JayLovesProgramming@users.noreply.github.com>
2024-09-15 16:52:50 +0100

    Made use of a namespace so Github colours it 😜

--- doubleandsinglequotes.cpp
+++ doubleandsinglequotes.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
+// Define a namespace
+namespace Example {
+
+    void demonstrateQuotes() {
+        // Single quotes are used for single characters
+        char singleQuoteChar = 'A'; // 'A' is a single character
+        std::cout << "Single quoted character: " << singleQuoteChar << std::endl;
+
+        // Double quotes are used for strings (arrays of characters)
+        const char* doubleQuoteString = "Hello, World!"; // "Hello, World!" is a string literal
+        std::cout << "Double quoted string: " << doubleQuoteString << std::endl;
+
+        // Demonstrating that single quotes can't be used for strings
+        // Uncommenting the following line will cause a compilation error
+        // char invalidString[] = 'This is not valid'; // Error: single quotes cannot be used for strings
+
+        // Demonstrating that double quotes can't be used for single characters
+        // Uncommenting the following line will cause a compilation error
+        // char invalidChar = "A"; // Error: double quotes are used for strings, not single characters
+    }
+
+} // namespace Example
+
 int main() {
-    // Single quotes are used for single characters
-    char singleQuoteChar = 'A'; // 'A' is a single character
-    std::cout << "Single quoted character: " << singleQuoteChar << std::endl;
-
-    // Double quotes are used for strings (arrays of characters)
-    const char* doubleQuoteString = "Hello, World!"; // "Hello, World!" is a string literal
-    std::cout << "Double quoted string: " << doubleQuoteString << std::endl;
-
-    // Demonstrating that single quotes can't be used for strings
-    // Uncommenting the following line will cause a compilation error
-    // char invalidString[] = 'This is not valid'; // Error: single quotes cannot be used for strings
-
-    // Demonstrating that double quotes can't be used for single characters
-    // Uncommenting the following line will cause a compilation error
-    // char invalidChar = "A"; // Error: double quotes are used for strings, not single characters
-
+    Example::demonstrateQuotes();
     return 0;
 }
