c3f8cfb5ea8aef51f1ec37ed45cf5d1ef2f85d52
Jay <116266395+JayLovesProgramming@users.noreply.github.com>
2024-09-15 16:50:57 +0100

    Create doubleandsinglequotes.cpp

---
+++ doubleandsinglequotes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Single quotes are used for single characters
+    char singleQuoteChar = 'A'; // 'A' is a single character
+    std::cout << "Single quoted character: " << singleQuoteChar << std::endl;
+
+    // Double quotes are used for strings (arrays of characters)
+    const char* doubleQuoteString = "Hello, World!"; // "Hello, World!" is a string literal
+    std::cout << "Double quoted string: " << doubleQuoteString << std::endl;
+
+    // Demonstrating that single quotes can't be used for strings
+    // Uncommenting the following line will cause a compilation error
+    // char invalidString[] = 'This is not valid'; // Error: single quotes cannot be used for strings
+
+    // Demonstrating that double quotes can't be used for single characters
+    // Uncommenting the following line will cause a compilation error
+    // char invalidChar = "A"; // Error: double quotes are used for strings, not single characters
+
+    return 0;
+}
